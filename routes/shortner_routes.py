5730617c93fd82417f5a336ada662eac25bf4d02
Mushfiqur Rahman <mushfiqurniaz33@gmail.com>
2025-06-14 23:01:06 +0600

    added get shortened route

--- routes/shortner_routes.py
+++ routes/shortner_routes.py
@@ -8,19 +8,19 @@ from utils.redis_client import redis_client
 router = APIRouter()
 
 
-# api route function for the creation the short url from a long url
+# api route function for the creation of the short url from a long url
 @router.post("/shortner/")
 async def create_short_url(request: Request, response: Response):
-    # access the request body of the in the request
-    req_body = await request.body()
+    # access the request body of data in the request
+    req_body = await request.json()
 
     try:
         # validate the request body
-        validated_req_body = CreateShortURLValidator(url=req_body)
+        validated_req_body = CreateShortURLValidator(url=req_body.get("url").strip())
 
     except ValidationError as e:
 
-        response.status_code = 404
+        response.status_code = 401
         return {
             "status": "error",
             "message": "Failed in type validation.",
@@ -58,3 +58,41 @@ async def create_short_url(request: Request, response: Response):
             "status": "error",
             "message": "Something went wrong.",
         }
+
+
+# api route function for getting the already created short url by providing the long url
+@router.get("/shortner/")
+async def get_short_url(request: Request, response: Response):
+    # access the request body of the sent request from the client
+    req_body = await request.json()
+
+    try:
+        # validate the request body using the create short url validator as it has the same value in both the route functions
+        validated_req_body = CreateShortURLValidator(url=req_body.get("url").strip())
+
+    except ValidationError as e:
+
+        response.status_code = 401
+        return {
+            "status": "error",
+            "message": "Failed in type validation.",
+            "errors": e.errors(),
+        }
+
+    try:
+        # retrieve the shortened url in the redis database if not exists send back error message
+        check_shortened_url_exists = redis_client.get(validated_req_body.url)
+        
+        if check_shortened_url_exists == None:
+            response.status_code = 404
+            return {"status": "error", "message": "Shortened URL was not found."}
+
+        response.status_code = 200
+        return {
+            "status": "success",
+            "message": "Shortened URL was found.",
+            "data": {"shortened_url": f"http://8000/{check_shortened_url_exists}"},
+        }
+    except Exception:
+        response.status_code = 500
+        return {"status": "error", "message": "Something went wrong."}
--- validators/shortner_validators.py
+++ validators/shortner_validators.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, Field
 
-
 class CreateShortURLValidator(BaseModel):
     url: str = Field(min_length=8, max_length=2000)
